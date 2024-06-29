97fd21265313d0676c8cb0287f0412745ba88747
nsavinda <nirmalsavinda29@gmail.com>
2024-06-29 22:47:35 +0530

    fix:log elk

--- app/main.py
+++ app/main.py
@@ -1,4 +1,31 @@
 from fastapi import FastAPI
+import logging
+from logstash_async.handler import AsynchronousLogstashHandler
+
+host = 'logstash'
+port = 5000
+
+# Create the logger
+logger = logging.getLogger("python-logstash-logger")
+logger.setLevel(logging.INFO)
+
+# Create the handler
+handler = AsynchronousLogstashHandler(host, port, database_path='')
+logger.addHandler(handler)
+
+# Create the formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add the formatter to the handler
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(handler)
+
+# Test the logger
+logger.info("Testify AI Application Started")
+
+
 
 # Create an instance of the FastAPI application with a custom title
 app = FastAPI(title="Testify AI")
@@ -11,4 +38,7 @@ async def read_root() -> dict:
     Returns:
         dict: A dictionary containing a greeting message.
     """
+
+    logger.info("Root endpoint accessed")
+
     return {"message": "Welcome to the Testify AI Assistant!"}
